5aba3945743c5ccf0acffe8bc4a5f76e5ce8572e
heha-proglea <heha965+proglea@gmail.com>
2018-11-16 21:34:16 +0000

    Add comments

--- app/entry.js
+++ app/entry.js
@@ -1,8 +1,14 @@
+// ----- HTMLに組み込まれるJavaScriptファイル -----
+
 'use strict';
+
+// ダメージ計算モジュールを用いて、HTMLbodyに計算結果を出力
 import dc from 'damage-calc';
-import crypto from 'crypto';
 document.writeln(
   '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
-  dc.effectiveDamage(100, 50, 30) + '</p>');
+    dc.effectiveDamage(100, 50, 30) + '</p>');
+
+// cryptoモジュールを用いて、HTMLbodyにランダム文字列を出力
+import crypto from 'crypto';
 document.writeln(
   '<p>' + crypto.randomBytes(8).toString('hex') + '</p>');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
+// ----- webpackの設定ファイル -----
+
 module.exports = {
+  // クライアントのJavaScriptが含まれるディレクトリの設定
   context: __dirname + '/app',
+  // メイン(=依存関係の起点)となるJavaScriptファイルの指定 (※ファイル名の'.js'は省略してもよい)
   entry: './entry',
+  // まとめられたJavaScriptの出力先となるディレクトリ&JavaScriptファイル名
   output: {
     path: __dirname + '/public/javascripts',
     filename: 'bundle.js'
